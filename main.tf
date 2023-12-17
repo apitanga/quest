13a03c3aa4bd91e4f22e731f29d2357dba017d82
André Pitanga <android.pitanga@gmail.com>
2023-12-16 23:08:31 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  cloud {
+    organization = "Pitangaville"
+
+    workspaces {
+      name = "quest"
+    }
+  }
+}
+# Provider configuration
+provider "google" {
+  project = "quest-408401"
+  region = "us-central1"
+}
+
+# Artifact Registry Repository
+resource "google_artifact_registry_repository" "docker_repo" {
+  location      = "us-central1"
+  repository_id = "quest"
+  format        = "DOCKER"
+
+  labels = {
+    env = "production"
+  }
+}
