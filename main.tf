af205faca87d71ed2eff30e206b488f4f4120cf8
André Pitanga <android.pitanga@gmail.com>
2023-12-17 21:22:27 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ resource "google_artifact_registry_repository" "docker_repo" {
 
 # Docker image to push
 resource "docker_image" "app_image" {
-  name  = "gcr.io/${gcp_project}/${google_artifact_registry_repository.docker_repo.repository_id}:${var.GITHUB_SHA}"
+  name  = "gcr.io/${gcp_project}/${google_artifact_registry_repository.docker_repo.repository_id}:${var.github_sha}"
 
   build_context = "."
   filename      = "Dockerfile"
