31bb78b6c741bc2c7e41d0cc71c7cd61c0248ea5
André Pitanga <android.pitanga@gmail.com>
2023-12-17 19:41:29 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,3 +27,33 @@ resource "google_artifact_registry_repository" "docker_repo" {
   output "url" {
     value = self.url
 }
+  # Get the repository URL as output
+  output "url" {
+    value = self.url
+  }
+}
+
+# Docker image to push
+resource "docker_image" "app_image" {
+  name  = "gcr.io/${google.project}/${google_artifact_registry_repository.docker_repo.repository_id}:${github.sha}"
+
+  build_context = "."
+
+  # Use local Dockerfile
+  filename = "Dockerfile"
+
+  depends_on = [
+    google_artifact_registry_repository.docker_repo,
+  ]
+}
+
+# Push the image to the repository
+resource "google_artifact_registry_repository_image" "pushed_image" {
+  location      = google_artifact_registry_repository.docker_repo.location
+  repository_id = google_artifact_registry_repository.docker_repo.repository_id
+  name         = docker_image.app_image.name
+
+  depends_on = [
+    docker_image.app_image,
+  ]
+}
