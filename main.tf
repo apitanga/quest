1a736504aad93b1d5dcf38975ec13f3e851432dd
André Pitanga <android.pitanga@gmail.com>
2023-12-17 21:01:14 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,12 +30,10 @@ resource "google_artifact_registry_repository" "docker_repo" {
 
 # Docker image to push
 resource "docker_image" "app_image" {
-  name  = "gcr.io/${google.project}/${google_artifact_registry_repository.docker_repo.repository_id}:${var.GITHUB_SHA}""
+  name  = "gcr.io/${google.project}/${google_artifact_registry_repository.docker_repo.repository_id}:${var.GITHUB_SHA}"
 
   build_context = "."
-
-  # Use local Dockerfile
-  filename = "Dockerfile"
+  filename      = "Dockerfile"
 
   depends_on = [
     google_artifact_registry_repository.docker_repo,
